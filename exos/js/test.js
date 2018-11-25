a1d0f2298d4b985f27cd809bcec569fb4eee4a86
cri <marticri@gmail.com>
2018-11-25 17:59:26 +0100

    base site de ben

---
+++ exos/js/test.js
@@ -0,0 +1,25 @@
+var nick;
+var array = [];
+
+while (true) {
+  nick = prompt("Entrez un prénom :");
+
+  if (nick) {
+    array.push(nick);
+  } else {522222222222222222222
+
+    break; // On quitte la boucle
+  }
+}
+if (!array[0]) {
+  alert("vous n'avez pas rentré de prenoms!");
+
+}else{
+  var list = array.join(" ");
+  alert(list);
+}
+
+https://openclassrooms.com/fr/courses/1916641-dynamisez-vos-sites-web-avec-javascript/2724891-deboguer-votre-code-partie-1-3#/id/r-2725027
+https://openclassrooms.com/fr/courses/1916641-dynamisez-vos-sites-web-avec-javascript/2724891-deboguer-votre-code-partie-1-3#/id/r-2725023
+https://openclassrooms.com/fr/courses/1916641-dynamisez-vos-sites-web-avec-javascript/2724891-deboguer-votre-code-partie-1-3#/id/r-2725024
+https://openclassrooms.com/fr/courses/1916641-dynamisez-vos-sites-web-avec-javascript/2724891-deboguer-votre-code-partie-1-3#/id/r-2725026
