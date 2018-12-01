640ee41b68a878b3cd25b4caf4a41fd61daddf84
cri <marticri@gmail.com>
2018-12-01 22:33:17 +0100

    add ben2 repository

---
+++ ben/nav.php
@@ -0,0 +1,29 @@
+<ul>
+            <li>
+              <span class="menu_titre">Menu</span>
+            </li>
+            <li>
+              <a id="menu" href="siteBen.html">Accueil ( News!)</a>
+            </li>
+            <li>
+              <a id="menu" href="collectif.html">Le collectif</a>
+            </li>
+            <li>
+              <a id="menu" href="#">Les recettes</a>
+            </li>
+            <li>
+              <a id="menu" href="#">Contact</a>
+            </li>
+            <li>
+              <a id="menu" href="#">Calendrier de nos brassins</a>
+            </li>
+            <li>
+              <a id="menu" href="#">Tuto "Bière à la maison"</a>
+            </li>
+            <li>
+              <a id="menu" href="#">Les Ateliers découverte brassage</a>
+            </li>
+            <li>
+              <a id="menu" href="#">PAF</a>
+            </li>
+          </ul>
--- ben/siteBen.php
+++ ben/siteBen.php
@@ -21,36 +21,7 @@
         <div id="aside_top"></div>
 
         <aside id="aside_menu">
-          <ul>
-            <li>
-              <span class="menu_titre">Menu</span>
-            </li>
-            <li>
-              <a id="menu" href="siteBen.html">Accueil ( News!)</a>
-            </li>
-            <li>
-              <a id="menu" href="collectif.html">Le collectif</a>
-            </li>
-            <li>
-              <a id="menu" href="#">Les recettes</a>
-            </li>
-            <li>
-              <a id="menu" href="#">Contact</a>
-            </li>
-            <li>
-              <a id="menu" href="#">Calendrier de nos brassins</a>
-            </li>
-            <li>
-              <a id="menu" href="#">Tuto "Bière à la maison"</a>
-            </li>
-            <li>
-              <a id="menu" href="#">Les Ateliers découverte brassage</a>
-            </li>
-            <li>
-              <a id="menu" href="#">PAF</a>
-            </li>
-
-          </ul>
+          <?php include ("navigation.php"); ?>
 
         </aside>
       </div>
